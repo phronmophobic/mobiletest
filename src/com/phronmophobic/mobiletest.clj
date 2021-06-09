eee4bcb4c6d52af069046bd9508e7aadb8c0bd2d
Adrian Smith <smith.adriane@gmail.com>
2021-06-09 11:24:59 -0700

    clean up clojure file.

--- src/com/phronmophobic/mobiletest.clj
+++ src/com/phronmophobic/mobiletest.clj
@@ -1,34 +1,30 @@
 (ns com.phronmophobic.mobiletest)
 
-(defn clj-sub [a b]
+(defn clj_sub [a b]
   (- a b))
 
+(defn clj_add [a b]
+  (+ a b))
 
-(comment
-  (do
-    (require '[tech.v3.datatype.ffi.graalvm :as graalvm])
-    (with-bindings {#'*compile-path* "library/classes"}
-      (graalvm/expose-clojure-functions
-       ;;name conflict - initialize is too general
-       {;;#'initialize-avclj {:rettype :int64}
-        #'clj-sub {:rettype :int64
-                   :argtypes [['a :int64]
-                              ['b :int64]
-                              ;['out-fname :pointer]
-                              ;['input-pixfmt :pointer]
-                              ]}
-        ;; #'is-avclj-initialized {:rettype :int64}
-        ;; #'make-h264-encoder {:rettype :int64
-        ;;                      :argtypes [['height :int64]
-        ;;                                 ['width :int64]
-        ;;                                 ['out-fname :pointer]
-        ;;                                 ['input-pixfmt :pointer]]}
-        ;; #'encode-frame {:rettype :int64
-        ;;                 :argtypes [['encoder :int64]
-        ;;                            ['frame-data :pointer]
-        ;;                            ['frame-data-len :int64]]}
-        ;; #'close-encoder {:rettype :int64
-        ;;                  :argtypes [['encoder :int64]]}
-        } 
-       'com.phonemophobic/MobileTest nil)))
+
+(defn compile-interface-class
+  ([]
+   (compile-interface-class nil))
+  ([opts]
+   (with-bindings {#'*compile-path* "library/classes"}
+     ((requiring-resolve 'tech.v3.datatype.ffi.graalvm/expose-clojure-functions)
+      ;;name conflict - initialize is too general
+      { ;;#'initialize-avclj {:rettype :int64}
+       #'clj_sub {:rettype :int64
+                  :argtypes [['a :int64]
+                             ['b :int64]
+                             ]}
+
+       #'clj_add {:rettype :int64
+                 :argtypes [['a :int64]
+                            ['b :int64]]}
+
+       }
+      'com.phonemophobic/MobileTest nil)))
   )
+
